56315bcab653a3af4445f673f2fe9a932740c001
Nayyab Younas Khan <146915176+nayyabyounaskhan@users.noreply.github.com>
2023-11-26 14:21:34 +0000

    updated is_prime code

--- is_prime.py
+++ is_prime.py
@@ -1,12 +1,17 @@
 #code to check if the entered number is prime or not?
 
-number=input(int())
+number=input( )
 def is_prime(number):
-    number=int(number)
+    number=int(number)  #to convert str to int
     if number < 2:
-        return False
+        print('Number less than 2')
+        return  #exit the function if number is less than 2
+    
     for i in range(2, int(number**0.5) + 1):
         if number % i == 0:
-            return False
-    return True
+            print(f'{number} is not a prime number')
+            return  #exit the function if number is not prime
+        
+    print(f'{number} is a prime number')
+    
 is_prime(number)
